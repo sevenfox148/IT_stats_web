b405821732486cbe83dc7fcfb40158e9ef67ee86
Semenova Yelyzaveta <sevenfox148@gmail.com>
2024-12-15 02:44:25 +0200

    Wait till database loaded

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
     scheduler.start()
 
     print("Waiting for data to load...")
-    time.sleep(60)
+    time.sleep(30)
 
     print("Starting Flask app...")
     app.run(debug=True)
